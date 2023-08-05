df3cce4ee163d779b90e1149e6de8fd7f306bbf6
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-04 18:46:24 -0700

    Added features and animation to header

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './Header.scss';
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 3 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+           <div className="badge-cmp app__flex">
+            <span>😊</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello I am </p>
+              <h1 className="head-text">Matthew</h1>
+            </div>
+           </div>
+
+           <div className="tag-cmp app__flex">
+            <p className="p-text">Software Engineer</p>
+            <p className="p-text">Web Developer</p>
+           </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile-bg" />
+      </motion.div>
+
+      <motion.div>
+
+      </motion.div>
+    </div>
   );
 }
 
