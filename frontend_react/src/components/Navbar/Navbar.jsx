488f6ffe2a811490f22f2ec108cb46afbed76ce7
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-04 18:53:36 -0700

    Changed logo in navigation bar to div for now

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,8 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        {/* <img src={images.logo} alt="logo" /> */}
+        <h1>Matthew Kwon</h1>
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
