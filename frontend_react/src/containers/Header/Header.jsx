786355b1696c3f71813ee11c2a61ca72af254f89
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-08 21:05:47 -0700

    Updated Header section with relevant technologies

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
       >
         <div className="app__header-badge">
            <div className="badge-cmp app__flex">
-            <span>😊</span>
+            {/* <span>😊</span> */}
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello I am </p>
               <h1 className="head-text">Matthew</h1>
@@ -35,7 +35,8 @@ const Header = () => {
 
            <div className="tag-cmp app__flex">
             <p className="p-text">Software Engineer</p>
-            <p className="p-text">Web Developer</p>
+            {/* <p className="p-text">Web Developer</p>
+            <p className="p-text">Programmer</p> */}
            </div>
         </div>
       </motion.div>
@@ -61,7 +62,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.node, images.javascript, images.react, images.redux, images.html, images.css].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
