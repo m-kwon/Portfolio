fa0517e1bb7a1276cd8c9365d4a081c6cedb9943
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-16 20:55:59 -0700

    Changed header slightly

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -35,8 +35,7 @@ const Header = () => {
 
            <div className="tag-cmp app__flex">
             <p className="p-text">Software Engineer</p>
-            {/* <p className="p-text">Web Developer</p>
-            <p className="p-text">Programmer</p> */}
+            <p className="p-text">Web Developer</p>
            </div>
         </div>
       </motion.div>
