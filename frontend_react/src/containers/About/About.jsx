fa165fd45fbd71fa6ee4b8b685b0b2a42107d373
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-16 20:55:19 -0700

    Added basic layout for About section

--- frontend_react/src/containers/About/About.jsx
+++ frontend_react/src/containers/About/About.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './About.scss';
+
+const abouts = [
+  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
+  { title: 'Frontend', description: 'I am a good web developer.', imgUrl: images.about02 },
+  { title: 'Backend', description: 'I am a good web developer.', imgUrl: images.about03 },
+  { title: 'Fullstack', description: 'I am a good web developer.', imgUrl: images.about04 }
+]
 
 const About = () => {
   return (
-    <h1>About</h1>
+    <>
+      <h2 className="head-text">
+        I know that
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__orifuke0ten"
+            key={about.title + index}
+          >
+            <img srv={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
 
