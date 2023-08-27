ea63009569ad60372980dc2b3102caf102646dc3
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-08-26 19:28:36 -0700

    Trimmed down and added needed icon assets

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { BsGithub, BsLinkedin, BsChatRightText } from 'react-icons/bs';
 
 const SocialMedia = () => {
+  const handleClick = () => {
+
+  }
+
   return (
     <div className="app__social">
-      <div>
-        <BsChatRightText />
-      </div>
-      <div>
-        <BsLinkedin />
-      </div>
-      <div>
-        <BsGithub />
-      </div>
+      <a href="mailto:matthew.o.kwon@gmail.com" target="_blank">
+        <div>
+          <BsChatRightText />
+        </div>
+      </a>
+      <a href="https://www.linkedin.com/in/matthewkwon/" target="_blank">
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a href="https://github.com/m-kwon" target="_blank">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
     </div>
   )
 }
